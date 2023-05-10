27986720ef67e99dd200c375bb646b13a878eccc
GANESHKUMARSAHOO <105813871+GANESHALIAS@users.noreply.github.com>
2023-05-10 23:27:16 +0530

    Time: 48 ms (16.94%), Space: 16.4 MB (11.97%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -4,15 +4,10 @@ class Solution:
         if len(strs)==1:
             return strs[0]
         else:
-            strs.sort()
-            print(strs)
             x=strs[0]
-            print(x)
             for i in range(0,len(x)+1):
                 k=x[:i]
-                print(k)
                 for j in range(1,len(strs)):
-                    print(strs[j])
                     if k == strs[j][:i]:
                         pass
                     else:
