1217df87026e22fa171abc5e30b458dec6e77f93
GANESHKUMARSAHOO <105813871+GANESHALIAS@users.noreply.github.com>
2023-05-12 00:02:20 +0530

    Time: 72 ms (15.61%), Space: 17 MB (20.14%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        a=""
+        s=s.lower()
+        s=s.replace(" ","")
+        for i in s:
+            if i.isalnum():
+                a+=i
+        
+        if a==a[::-1]:
+            return True
+        else:
+            return False
