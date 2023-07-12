8292f6d1160e078334173bdb5965556543518b88
GANESHKUMARSAHOO <105813871+GANESHALIAS@users.noreply.github.com>
2023-07-13 02:00:27 +0530

    Time: 43 ms (86.78%), Space: 16.3 MB (69.61%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.py
+++ 2315-count-asterisks/2315-count-asterisks.py
@@ -1,13 +1,13 @@
 import collections
 class Solution:
     def countAsterisks(self, s: str) -> int:
-        k=s.split("|")
-        i=1
-        while i<len(k):
-            k[i]=""
-            i+=2
-        k="".join(k)
-        c=collections.Counter(k)
-        
-        return c["*"]
+        count=0
+        ans=0
+        for i in s:
+            if i == "|":
+                count+=1
+            elif count%2==0:
+                if i == "*":
+                    ans+=1
+        return ans
         
