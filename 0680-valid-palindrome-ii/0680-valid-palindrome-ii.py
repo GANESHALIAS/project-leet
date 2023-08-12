ef58f69e495e006d8c6b9ddf8638e294a8e3802d
GANESHKUMARSAHOO <105813871+GANESHALIAS@users.noreply.github.com>
2023-08-12 22:40:22 +0530

    Time: 76 ms (97.58%), Space: 16.8 MB (76.54%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,14 +1,15 @@
 class Solution:
-    
-
     def validPalindrome(self, s: str) -> bool:
+        if s == s[::-1]:
+            return True
+        else:
             st=0
             en=len(s)-1
-            while st<=en:
+            while st<en:
                 if s[st] != s[en]:
                     str1 = s[:st]+s[st+1:]
                     str2 = s[:en]+s[en+1:]
                     return str1==str1[::-1] or str2==str2[::-1]
                 st+=1
                 en-=1
-            return True
+            
