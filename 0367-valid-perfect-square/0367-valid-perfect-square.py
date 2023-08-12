ef52afd644b5820775c4896179f84ed49f45d2b9
GANESHKUMARSAHOO <105813871+GANESHALIAS@users.noreply.github.com>
2023-08-12 22:00:58 +0530

    Time: 36 ms (89.55%), Space: 16.3 MB (28.05%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -1,8 +1,8 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
         k=pow(num,0.5)
-        m=float(int(k))
-        if m== k:
+        m=int(k)
+        if m == k:
             return True
         else:
             return False
