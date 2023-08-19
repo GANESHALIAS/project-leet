704cc6e69966337fee3e359494f2eeaf79c24e4d
GANESHKUMARSAHOO <105813871+GANESHALIAS@users.noreply.github.com>
2023-08-19 19:31:28 +0530

    Time: 54 ms (87.29%), Space: 16.4 MB (24.81%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        
+        bits=bits[::-1]
+        bits=bits[1:]
+        bits=[ str(i) for i in bits]
+        s="".join(bits)
+        s=s.replace("11","")
+        s=s.replace("01","")
+        if len(s)!=0:
+            if "1" in s:
+                return False
+            else:
+                return True
+        else:
+            return True
